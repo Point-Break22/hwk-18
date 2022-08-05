b06a944e2e4bfb9456902eb1ca512bfd6478ebb2
Point-Break22 <crystalsjgayle@gmail.com>
2022-08-05 13:00:39 -0400

    Css interaction
    added CSS button interaction
    added case study photos
    shrunk down social link logos
    changed navigation removed them as buttons

---
+++ JS/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+// Watch the Button tutorial
+// http://react.school/ui/button
+
+// Free React training
+// http://react.school/join
+
+// Free Material-UI template
+// http://react.school/material-ui/templates
+
+const theme = {
+  blue: {
+    default: "#3f51b5",
+    hover: "#283593"
+  },
+  pink: {
+    default: "#e91e63",
+    hover: "#ad1457"
+  }
+};
